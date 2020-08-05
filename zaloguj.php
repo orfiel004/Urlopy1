9a497bfc96165b609b53a7adb5f7c381d11fc7ff
orfiel <pawel.sleczek@gmail.com>
2020-08-05 23:48:38 +0200

    Connection to DB works

--- zaloguj.php
+++ zaloguj.php
@@ -1,10 +1,27 @@
 <?php
 
-  $login = $_POST['login'];
-  $haslo = $_POST['haslo'];
+  //załączenie pliku z informacjami o DB
+  require_once "connect.php";
 
-  echo $login."<br/>";
-  echo $haslo
-;
+  //stworzenie obiektu reprezentującego połączenie w kodzie i za pomocą jego instancji ustanowienie połałczeia z bazą
+  //przed new "@" wycisza wyświetlanie błędów
+  $connection = @new mysqli($host, $db_user, $db_password, $db_name);
+
+  if($connection->connect_errno!=0)
+  {
+    echo "Error".$connection->connect_errno." Opis ".$connectio->connect_error;
+  }
+  else
+  {
+
+    $login = $_POST['login'];
+    $haslo = $_POST['haslo'];
+
+    echo "It works!";
+
+    $connection->close();
+  }
+
+
 
  ?>
