becd14485084d34d3a8ddefa39a780e1f521f035
orfiel <pawel.sleczek@gmail.com>
2020-08-05 22:09:22 +0200

    Loging Page run

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-Hello World
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>Aplikacja urlopowa</title>
+</head>
+
+<body>
+  Zaloguj jako Admin<br/><br/>
+
+  <form action="zaloguj.php" method="post">
+
+    Login: <br/> <input type="text" name="login" <br/>
+    Hasło: <br/> <input type="password" name="hasło" <br/>
+    <input type="submit" value="Zaloguj">
+
+
+  </form>
+
+
+</body>
+
+</html>
